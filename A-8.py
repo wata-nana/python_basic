38702a2b4640a7475f87e3342472d0984a95d856
wata-nana <tonarinotototro@gmail.com>
2025-08-21 12:35:28 +0900

    A-8修正

--- A-8.py
+++ A-8.py
@@ -1,6 +1,5 @@
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
-for i in range(len(users_info)):
-    user = users_info[i]
+for user in users_info:
     profile = "Name: {}, Age: {}".format(*user)
     print(profile)
